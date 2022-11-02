3ef45ef93f104345043421949102399698e84668
grbravo <grbravo@ucm.es>
2022-11-02 21:54:57 +0100

    codigo prueba 1

--- data/mostrar_defectos.py
+++ data/mostrar_defectos.py
@@ -1,14 +1,34 @@
 import cv2
 import matplotlib.pyplot as plt
+import pandas as pd
 
-#Cargar la imagen con openCV
 
-imgBGR = cv2.imread('C:/Users/Gbravove/PycharmProjects/tfg-gbv/data/gdxray/Castings/C0001/C0001_0001.png')
+# cargar groundthruth en dataframe
+df = pd.read_fwf('C:/Users/Gbravove/PycharmProjects/tfg-gbv/data/gdxray/Castings/C0001/ground_truth.txt',header=None)
+df.columns=["imagen", "x1", "x2", "y1","y2"]
+print(df)
 
-#Cambiar esacio de color BGR a RGB
-imgRGB = cv2.cvtColor(imgBGR, cv2.COLOR_BGR2RGB)
+# Cargar la imagen con openCV
+imagenBGR = cv2.imread('C:/Users/Gbravove/PycharmProjects/tfg-gbv/data/gdxray/Castings/C0001/C0001_0001.png')
+# Cambiar esacio de color BGR a RGB
+imagenRGB = cv2.cvtColor(imagenBGR, cv2.COLOR_BGR2RGB)
+# Mostrar imagen
+#plt.xticks([]), plt.yticks([])
+plt.imshow(imagenRGB)
+#plt.show()
 
-#Mostrar imagen
-plt.xticks([]), plt.yticks([])
-plt.imshow(imgBGR)
-plt.show()
+#Dibujando un rectangulo
+x1=int(df['x1'].values[0])
+y1=int(df.iloc[0]['y1'])
+x2=int(df.iloc[0]['x2'])
+y2=int(df.iloc[0]['y2'])
+
+cv2.rectangle(imagenRGB,(x1,y1),(x2,y2),(255,255,255),3)
+
+cv2.imshow('imagen',imagenRGB)
+cv2.waitKey(0)
+
+
+
+
+
